aa3a2f734c4f16f7556c0523136584465a6cdff9
Wilde <gutopicciani@yahoo.com.br>
2016-02-12 02:09:23 -0200

    Primeiro commit. Colocado o que vou trabalhar em cima.

---
+++ desktop/src/br/com/wilde/cg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.wilde.cg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.wilde.cg.Projection;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Projection(), config);
+	}
+}
