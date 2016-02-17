9a275e8d395324c0a38adf3b61e469db9aead4b3
Wilde <gutopicciani@yahoo.com.br>
2016-02-17 02:03:34 -0200

    Separando configurações em um arquivo separado

---
+++ core/src/br/com/wilde/cg/Config.java
@@ -0,0 +1,18 @@
+package br.com.wilde.cg;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector3;
+
+public class Config {
+
+	public static Vector3 DIRECAO_LUZ = new Vector3(-1f, -0.8f, -0.2f);
+	public static Color   COR_LUZ     = new Color(0.8f, 0.8f, 0.8f, 1f);
+	
+	public static Vector3 POSICAO_INICIAL_CAMERA    = new Vector3(10f, 10f, 10f); // Vetor partindo da origem, indica onde a câmera está 
+	public static float   ANGULO_DE_ABERTURA_CAMERA = 67f;
+	public static Vector3 FOCO_INICIAL_CAMERA       = new Vector3(0f, 0f, 0f);
+	public static float   DISTANCIA_MINIMA_CAMERA   = 1f;
+	public static float   DISTANCIA_MAXIMA_CAMERA   = 300f;
+	
+	
+}
--- core/src/br/com/wilde/cg/Projection.java
+++ core/src/br/com/wilde/cg/Projection.java
@@ -1,6 +1,6 @@
 package br.com.wilde.cg;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -15,9 +15,9 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
-public class Projection extends ApplicationAdapter {
+public class Projection implements ApplicationListener {
 	public Environment lights;
-	public PerspectiveCamera cam;
+	public PerspectiveCamera camera;
 	public ModelBatch modelBatch;
 	public Model model;
 	public ModelInstance instance;
@@ -26,17 +26,17 @@ public class Projection extends ApplicationAdapter {
 	public void create() {
 		
 		lights = new Environment();
-		lights.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-		lights.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		lights.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));		
+		lights.add(new DirectionalLight().set(Config.COR_LUZ , Config.DIRECAO_LUZ));
 
 		modelBatch = new ModelBatch();
 
-		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(10f, 10f, 10f);
-		cam.lookAt(0, 0, 0);
-		cam.near = 1f;
-		cam.far = 300f;
-		cam.update();
+		camera = new PerspectiveCamera(Config.ANGULO_DE_ABERTURA_CAMERA, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(Config.POSICAO_INICIAL_CAMERA);
+		camera.lookAt(Config.FOCO_INICIAL_CAMERA);
+		camera.near = Config.DISTANCIA_MAXIMA_CAMERA;
+		camera.far = Config.DISTANCIA_MAXIMA_CAMERA;
+		camera.update();
 
 		ModelBuilder modelBuilder = new ModelBuilder();
 		model = modelBuilder.createBox(5f, 5f, 5f, new Material(ColorAttribute.createDiffuse(Color.RED)),
@@ -49,7 +49,7 @@ public class Projection extends ApplicationAdapter {
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
  
-        modelBatch.begin(cam);
+        modelBatch.begin(camera);
         modelBatch.render(instance, lights);
         modelBatch.end();
 	}
